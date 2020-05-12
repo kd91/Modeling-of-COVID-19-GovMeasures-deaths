4d8b40fd09ea95b1aed2b426583a673f561e97b2
Khushbu Desai <khushbu_desai@Khushbus-MBP.hsd1.ca.comcast.net>
2020-05-11 17:44:07 -0700

    Adds new plots

--- Covid_Modeling_ITA.R
+++ Covid_Modeling_ITA.R
@@ -37,9 +37,6 @@ cor(df1[, 2:7], use = "pairwise.complete.obs", method = "pearson")
 y <- df1_italy$new_deaths[1:60]
 x <- df1_italy$tagged_day[1:60]
 
-# y for korea <- 
-# x for korea <- 
-
 # Define a Gaussian function (of four parameters)
 f <- function(x, theta)  { 
   m <- theta[1]; s <- theta[2]; a <- theta[3]; b <- theta[4];
@@ -63,14 +60,6 @@ c(m=m.0, s=s.0, a=a.0, b=b.0)
 fit <- nls(y ~ f(x,c(m,s,a,b)), data.frame(x,y), start=list(m=m.0, s=s.0, a=a.0, b=b.0))
 summary(fit)
 
-rls <- nlsResiduals(fit)
-plot(rls, which=0)
-#it's not a very good fit. 
-# points are dispersed equally above & below the line
-
-test.nlsResiduals(rls)
-confint(fit)
-
 #interpretation:
 # a*exp(-0.5*((x-m)/s)^2) + b
 #850.5028*exp(-0.5*((x-44.4252)/19.4156)^2)  -124.2956
@@ -85,19 +74,32 @@ df1_italy$new_deaths[fit_peak_loc]
 # future days predictions for the curve
 italy_pred <- 850.5028*exp(-0.5*((df1_italy$tagged_day[61:75]-44.4252)/19.4156)^2)  -124.2956
 italy_pred
+italy_pred <- ceiling(italy_pred)
 
-PredVsActual <- data.frame(df1_italy$tagged_day[61:75],df1_italy$new_deaths[61:75], italy_pred)
+# model evaluation fro residuals and MAE
+rls <- nlsResiduals(fit)
+plot(rls, which=0)
+#it's not a very good fit. 
+# points are dispersed equally above & below the line
+MAE(italy_pred, df1_italy$new_deaths[61:75])
+# test.nlsResiduals(rls)
+
+# vector with 1-60 days actual deaths data & 61-75days predicted deaths data
+actual1.60_pred61.75 <- c(df1_italy$new_deaths[1:60], italy_pred)
+
+PredVsActual <- data.frame(df1_italy$tagged_day,df1_italy$new_deaths, actual1.60_pred61.75)
 PredVsActual
 
-ggplot() + geom_point(aes(x =PredVsActual$df1_italy.tagged_day.61.75., 
-                        y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) + 
-           geom_smooth(aes(x =PredVsActual$df1_italy.tagged_day.61.75., 
-                 y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) +
-          geom_point(aes(x =PredVsActual$df1_italy.tagged_day.61.75., 
-                 y = PredVsActual$italy_pred,colour = "predicted")) + 
-          geom_smooth(aes(x =PredVsActual$df1_italy.tagged_day.61.75., 
-                  y = PredVsActual$italy_pred,colour = "predicted")) + xlab("Tagged day") +
-            ylab("New_deaths") + ggtitle("Predicted vs Actual new_deaths for Italy")
+ggplot() +geom_point(aes(x =PredVsActual$df1_italy.tagged_day, 
+                 y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) + 
+          geom_smooth(aes(x =PredVsActual$df1_italy.tagged_day, 
+                  y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) +
+  geom_point(aes(x =PredVsActual$df1_italy.tagged_day, 
+                 y = PredVsActual$df1_italy.new_deaths,colour = "actual")) + 
+  geom_smooth(aes(x =PredVsActual$df1_italy.tagged_day, 
+                  y = PredVsActual$df1_italy.new_deaths,colour = "actual")) + 
+  xlab("Count of days (since the 20th confirmed case)") +
+            ylab("Daily new deaths") + ggtitle("Predicted vs Actual new deaths/day for Italy")
 
 ##############################################################################################################
 # for flights_china vs new_deaths
@@ -128,9 +130,6 @@ c(m=m.0, s=s.0, a=a.0, b=b.0)
 fit <- nls(y ~ f(x,c(m,s,a,b)), data.frame(x,y), start=list(m=m.0, s=s.0, a=a.0, b=b.0))
 summary(fit)
 
-plot(nlsResiduals(fit), which=0)
-#it's not a very good fit. points are not uniformly dispersed
-
 #interpretation:
 # a*exp(-0.5*((x-m)/s)^2) + b
 # 839.5095*exp(-0.5*((x-61.5959)/14.9453)^2)  -66.7616
@@ -145,19 +144,29 @@ df1_italy$new_deaths[fit_peak_loc]
 # future days predictions for the curve
 italy_pred <- 839.5095*exp(-0.5*((df1_italy$flights_china[61:75]-61.5959)/14.9453)^2)  -66.7616
 italy_pred
+italy_pred <- ceiling(italy_pred)
 
-PredVsActual <- data.frame(df1_italy$flights_china[61:75],df1_italy$new_deaths[61:75], italy_pred)
+# model evaluation for residuals and MAE
+plot(nlsResiduals(fit), which=0)
+#it's not a very good fit. points are not uniformly dispersed
+MAE(italy_pred, df1_italy$new_deaths[61:75])
+
+# vector with 1-60 days actual deaths data & 61-75days predicted deaths data
+actual1.60_pred61.75 <- c(df1_italy$new_deaths[1:60], italy_pred)
+
+PredVsActual <- data.frame(df1_italy$flights_china,df1_italy$new_deaths, actual1.60_pred61.75)
 PredVsActual
 
-ggplot() + geom_point(aes(x =PredVsActual$df1_italy.flights_china.61.75., 
-                          y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) + 
-  geom_smooth(aes(x =PredVsActual$df1_italy.flights_china.61.75., 
-                  y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) +
-  geom_point(aes(x =PredVsActual$df1_italy.flights_china.61.75., 
-                 y = PredVsActual$italy_pred,colour = "predicted")) + 
-  geom_smooth(aes(x =PredVsActual$df1_italy.flights_china.61.75., 
-                  y = PredVsActual$italy_pred,colour = "predicted")) + xlab("Days for flight restrictions from china") +
-  ylab("New_deaths") + ggtitle("Predicted vs Actual new_deaths for Italy")
+ggplot() + geom_point(aes(x =PredVsActual$df1_italy.flights_china, 
+                          y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) + 
+  geom_smooth(aes(x =PredVsActual$df1_italy.flights_china, 
+                  y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) + 
+  geom_point(aes(x =PredVsActual$df1_italy.flights_china, 
+                          y = PredVsActual$df1_italy.new_deaths,colour = "actual")) + 
+  geom_smooth(aes(x =PredVsActual$df1_italy.flights_china, 
+                  y = PredVsActual$df1_italy.new_deaths,colour = "actual")) +
+   xlab("# of Days since start of flight restrictions to/from china") +
+  ylab("Daily New Deaths") + ggtitle("Predicted vs Actual new deaths/day for Italy")
 
 ##############################################################################################################
 # for new_confirmed vs new_deaths
@@ -188,9 +197,6 @@ c(m=m.0, s=s.0, a=a.0, b=b.0)
 fit <- nls(y ~ f(x,c(m,s,a,b)), data.frame(x,y), start=list(m=m.0, s=s.0, a=a.0, b=b.0))
 summary(fit)
 
-plot(nlsResiduals(fit), which=0)
-#it's not a very good fit. points are not uniformly dispersed
-
 #interpretation:
 # a*exp(-0.5*((x-m)/s)^2) + b
 # 1215.1*exp(-0.5*((x-6652.5)/4645.6)^2)-464.2
@@ -205,17 +211,26 @@ df1_italy$new_deaths[fit_peak_loc]
 # future days predictions for the curve
 italy_pred <- 1215.1*exp(-0.5*((df1_italy$new_confirmed[61:75]-6652.5)/4645.6)^2) - 464.2
 italy_pred
+italy_pred <- ceiling(italy_pred)
 
-PredVsActual <- data.frame(df1_italy$new_confirmed[61:75],df1_italy$new_deaths[61:75], italy_pred)
+# model evaluation for residuals and MAE
+plot(nlsResiduals(fit), which=0)
+#it's not a very good fit. points are not uniformly dispersed
+MAE(italy_pred, df1_italy$new_deaths[61:75])
+
+# vector with 1-60 days actual deaths data & 61-75days predicted deaths data
+actual1.60_pred61.75 <- c(df1_italy$new_deaths[1:60], italy_pred)
+
+PredVsActual <- data.frame(df1_italy$new_confirmed,df1_italy$new_deaths, actual1.60_pred61.75)
 PredVsActual
 
-ggplot() + geom_point(aes(x =PredVsActual$df1_italy.new_confirmed.61.75., 
-                          y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) + 
-  geom_smooth(aes(x =PredVsActual$df1_italy.new_confirmed.61.75., 
-                  y = PredVsActual$df1_italy.new_deaths.61.75.,colour = "actual")) +
-  geom_point(aes(x =PredVsActual$df1_italy.new_confirmed.61.75., 
-                 y = PredVsActual$italy_pred,colour = "predicted")) + 
-  geom_smooth(aes(x =PredVsActual$df1_italy.new_confirmed.61.75., 
-                  y = PredVsActual$italy_pred,colour = "predicted")) + xlab("Number of confirmed cases") +
-  ylab("New_deaths") + ggtitle("Predicted vs Actual new_deaths for Italy")
-
+ggplot() + geom_point(aes(x =PredVsActual$df1_italy.new_confirmed, 
+                 y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) + 
+  geom_smooth(aes(x =PredVsActual$df1_italy.new_confirmed, 
+                  y = PredVsActual$actual1.60_pred61.75,colour = "predicted")) +
+  geom_point(aes(x =PredVsActual$df1_italy.new_confirmed, 
+                 y = PredVsActual$df1_italy.new_deaths,colour = "actual")) + 
+  geom_smooth(aes(x =PredVsActual$df1_italy.new_confirmed, 
+                  y = PredVsActual$df1_italy.new_deaths,colour = "actual")) +
+  xlab("Daily new confirmed cases") + ylab("Daily New Deaths") +
+  ggtitle("Predicted vs Actual new deaths/day for Italy")
